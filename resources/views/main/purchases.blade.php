832f05eda504bcc13e63598e69fc49d41acc28fa
Jan Rey Suarez <w33haa2@gmail.com>
2018-05-30 06:55:12 +0800

    a
    a

--- resources/views/main/purchases.blade.php
+++ resources/views/main/purchases.blade.php
@@ -194,7 +194,7 @@
                                        <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
                                             <div class="form-group">
                                                  <div class="form-line">
-                                                      <input type="partial" id="partial" name="sacks" class="form-control"   required>
+                                                      <input type="number" id="partial" name="partial" class="form-control"   required>
                                                  </div>
                                             </div>
                                        </div>
@@ -227,6 +227,20 @@
                                             </div>
                                        </div>
                                   </div>
+
+                                  <div class="row clearfix">
+                                       <div class="col-lg-2 col-md-2 col-sm-4 col-xs-5 form-control-label">
+                                            <label for="name">Remarks</label>
+                                       </div>
+                                       <div class="col-lg-10 col-md-10 col-sm-8 col-xs-7">
+                                            <div class="form-group">
+                                                 <div class="form-line">
+                                                      <input type="text" id="remarks" name="remarks" class="form-control"   required>
+                                                 </div>
+                                            </div>
+                                       </div>
+                                  </div>
+
 
 
                                   <div class="row clearfix">
