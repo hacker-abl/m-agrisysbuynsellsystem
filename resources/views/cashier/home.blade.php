bd78b215f1c6d4534c1ff7d7744472d490dd2477
Jan Rey Suarez <w33haa2@gmail.com>
2018-07-15 12:35:14 +0800

    test

--- resources/views/cashier/home.blade.php
+++ resources/views/cashier/home.blade.php
@@ -10,46 +10,17 @@
                <span>Home</span>
             </a>
         </li>
-        <li >
-            <a href="{{ route('expense') }}">
-               <i class="material-icons">show_chart</i>
-               <span>Expenses</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('trips') }}">
-               <i class="material-icons">directions_bus</i>
-               <span>Trips</span>
-            </a>
-        </li>
-        <li >
-            <a href="{{ route('dtr') }}">
-               <i class="material-icons">access_time</i>
-               <span>Daily Time Record</span>
-            </a>
-        </li>
-        <li >
-            <a href="{{ route('od') }}">
-               <i class="material-icons">arrow_upward</i>
-               <span>Outbound Deliveries</span>
-            </a>
-        </li>
+
         <li >
             <a href="{{ route('ca') }}">
                <i class="material-icons">monetization_on</i>
                <span>Cash Advance</span>
             </a>
         </li>
-        <li>
-            <a href="{{ route('purchases') }}">
-               <i class="material-icons">bookmark_border</i>
-               <span>Purchases</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('sales') }}">
-               <i class="material-icons">shopping_cart</i>
-               <span>Sales</span>
+        <li >
+            <a href="{{ route('customer') }}">
+                <i class="material-icons">tag_faces</i>
+                <span>Customer</span>
             </a>
         </li>
     </ul>
