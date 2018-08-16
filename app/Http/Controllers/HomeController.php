99c4c7e05146a5cbc9d08b876aefd40bd97c754f
unknown <floydignified@gmail.com>
2018-08-16 21:25:44 +0800

    homepage_way_pulos_nga_data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,20 @@ use Illuminate\Http\Request;
 use Auth;
 use App\User;
 use App\UserPermission;
+use App\paymentlogs;
+use App\customer;
+use App\commodity;
+use App\trucks;
+use App\purchases;
+use App\sales;
+use App\balance;
+use App\expense;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Query\Builder;
+use Response;
+use View;
 
 class HomeController extends Controller
 {
@@ -24,18 +38,33 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $id = Auth::user()->id;
         $user = User::find($id);
-        
+
+        $paymentLogs = paymentlogs::orderBy('id', 'desc')->paginate(5);
+        $commodityList = commodity::orderBy('id', 'desc')->paginate(6);
+        $truckList = trucks::orderBy('id', 'desc')->paginate(6);
+        $latestPurchases = purchases::orderBy('id', 'desc')->paginate(15);
+        $topCommodities = purchases::groupBy('commodity_id')
+            ->orderBy(DB::raw('SUM(total)'), 'desc')
+            ->limit(4)
+            ->get(['commodity_id', DB::raw('SUM(total) AS total')]);
+        $latestCustomer = customer::latest()->first();
+        $totalSales = sales::get([DB::raw('SUM(amount) AS total_sales')]);
+        $totalPurchases = purchases::get([DB::raw('SUM(total) AS total_purchases')]);
+        $totalBalance = balance::get([DB::raw('SUM(balance) AS total_balance')]);
+        $totalExpense = expense::get([DB::raw('SUM(amount) AS total_expense')]);
+
         if($user->role->name === "admin") {
-            return view('main.home');
+            return view('main.home', compact('paymentLogs', 'commodityList', 'truckList', 'latestPurchases', 'topCommodities', 'latestCustomer', 'totalSales', 'totalPurchases', 'totalBalance', 'totalExpense'));
         } else if($user->role->name === "user") {
             $permissions = UserPermission::with('permission')->get();
             
             return view('cashier.home', compact('permissions'));
         }
     }
+
 
 }
