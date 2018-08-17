38b64a62be85808ef8310ff0bfeffa37298d960a
BENBEN <benbenbadilles@gmail.com>
2018-08-17 21:10:37 +0800

    Revert "Merge branch 'master' into dynamicnavbar"
    This reverts commit 607db109b0a33cefec31a445c282e91eb280f5d0, reversing
    changes made to b96592ff0579c0bc3c84ae31d59ada57f9f71dac.

--- routes/web.php
+++ routes/web.php
@@ -82,11 +82,12 @@ Route::group(['middleware'=>['auth', 'user:expenses']], function() {
 Route::group(['middleware'=>['auth', 'user:trips']], function() {
     //TRIPS
     Route::get('/trips', 'tripController@index')->name('trips');
-    Route::get('/dtr', 'dtrController@index')->name('dtr');
     Route::post('/release_update', 'tripController@release_update')->name('release_update');
-    Route::post('/release_update_normal', 'expenseController@release_update_normal')->name('release_update_normal');
     Route::post('/release_update_dtr', 'dtrController@release_update_dtr')->name('release_update_dtr');
+    Route::post('/print_trip', 'pdfController@trips')->name('print_trip');
+});
 
+Route::group(['middleware'=>['auth', 'user:od']], function() {
     //OUTBOUND DELIVERIES
     Route::get('/outbound', 'odController@index')->name('od');
     Route::post('/refresh_deliveries', 'odController@refresh')->name('refresh_deliveries');
@@ -94,14 +95,20 @@ Route::group(['middleware'=>['auth', 'user:trips']], function() {
     Route::post('/add_delivery', 'odController@store')->name('add_delivery');
     Route::get('/update_delivery', 'odController@updatedata')->name('update_delivery');
     Route::get('/delete_delivery', 'odController@deletedata')->name('delete_delivery');
+    Route::post('/print_od', 'pdfController@od')->name('print_od');
+});
 
+Route::group(['middleware'=>['auth', 'user:sales']], function() {
     //Sales
     Route::get('/sales', 'salesController@index')->name('sales');
     Route::post('/refresh_sales', 'salesController@refresh')->name('refresh_sales');
     Route::post('/add_sales', 'salesController@store')->name('add_sales');
     Route::get('/update_sales', 'salesController@updatedata')->name('update_sales');
     Route::get('/delete_sales', 'salesController@deletedata')->name('delete_sales');
+    Route::post('/print_sales', 'pdfController@sales')->name('print_sales');
+});
 
+Route::group(['middleware'=>['auth', 'user:ca']], function() {
     //CASH ADVANCE
     Route::get('/cashadvance', 'caController@index')->name('ca');
     Route::post('/add_cashadvance', 'caController@store')->name('add_cashadvance');
@@ -111,7 +118,11 @@ Route::group(['middleware'=>['auth', 'user:trips']], function() {
     Route::get('/refresh_balancedt', 'balanceController@refresh')->name('refresh_balancedt');
     Route::get('/balancelogs', 'balanceController@balance')->name('balancelogs');
     Route::post('/add_payment', 'balanceController@store')->name('add_payment');
+    Route::post('/print_ca', 'pdfController@ca')->name('print_ca');
+    Route::post('/print_balance_payment', 'pdfController@balance_payment')->name('print_balance_payment');
+});
 
+Route::group(['middleware'=>['auth', 'user:purchases']], function() {
     //PURCHASES
     Route::get('/purchases', 'purchasesController@index')->name('purchases');
     Route::get('/find_amt', 'purchasesController@findAmount')->name('find_amt');
@@ -120,7 +131,10 @@ Route::group(['middleware'=>['auth', 'user:trips']], function() {
     Route::get('/find_comm', 'purchasesController@findcomm')->name('find_comm');
     Route::post('/add_purchases', 'purchasesController@store')->name('add_purchases');
     Route::post('/refresh_purchases', 'purchasesController@refresh')->name('refresh_purchases');
+    Route::post('/print_purchase', 'pdfController@purchases')->name('print_purchase');
+});
 
+Route::group(['middleware'=>['auth', 'user:dtr']], function() {
     //DTR
     Route::get('/check_employee', 'dtrController@check_employee')->name('check_employee');
     Route::get('/dtr_details', 'dtrController@dtr_details')->name('dtr_details');
@@ -128,7 +142,11 @@ Route::group(['middleware'=>['auth', 'user:trips']], function() {
     Route::get('/refresh_view_dtr', 'dtrController@refresh_view')->name('refresh_view_dtr');
     Route::post('/add_dtr', 'dtrController@store')->name('add_dtr');
     Route::post('/add_dtr_expense', 'dtrController@add_dtr_expense')->name('add_dtr_expense');
+    Route::get('/dtr', 'dtrController@index')->name('dtr');
+    Route::post('/print_dtr', 'pdfController@dtr')->name('print_dtr');
+});
 
+Route::group(['middleware'=>['auth', 'user:trips']], function() {
     //PICK UP
     Route::get('/get_pickup', 'tripController@updateId')->name('get_pickup');
     Route::post('/refresh_pickup', 'tripController@refresh')->name('refresh_pickup');
@@ -136,67 +154,9 @@ Route::group(['middleware'=>['auth', 'user:trips']], function() {
     Route::get('/delete_trip', 'tripController@deletedata')->name('delete_trip');
     Route::post('/add_pickup', 'tripController@store')->name('add_pickup');
     Route::post('/update_trip', 'tripController@update_trip')->name('update_trip');
-
-
-    //settings
-    Route::get('/company', 'companyController@index')->name('company');
-    Route::post('/add_company', 'companyController@store')->name('add_company');
-    Route::get('/refresh_company', 'companyController@refresh')->name('refresh_company');
-    Route::get('/update_company', 'companyController@updatedata')->name('update_company');
-    Route::get('/delete_company', 'companyController@deletedata')->name('delete_company');
-
-    Route::get('/employee', 'employeeController@index')->name('employee');
-    Route::post('/add_employee', 'employeeController@store')->name('add_employee');
-    Route::get('/refresh_employee', 'employeeController@refresh')->name('refresh_employee');
-    Route::get('/update_employee', 'employeeController@updatedata')->name('update_employee');
-    Route::get('/delete_employee', 'employeeController@deletedata')->name('delete_employee');
-
-    Route::get('/customer', 'customerController@index')->name('customer');
-    Route::post('/add_customer', 'customerController@store')->name('add_customer');
-    Route::get('/refresh_customer', 'customerController@refresh')->name('refresh_customer');
-    Route::get('/update_customer', 'customerController@updatedata')->name('update_customer');
-    Route::get('/delete_customer', 'customerController@deletedata')->name('delete_customer');
-    Route::get('/refresh_balance', 'customerController@updateId')->name('refresh_balance');
-
-    Route::get('/trucks', 'trucksController@index')->name('trucks');
-    Route::post('/add_trucks', 'trucksController@store')->name('add_trucks');
-    Route::get('/refresh_trucks', 'trucksController@refresh')->name('refresh_trucks');
-    Route::get('/update_trucks', 'trucksController@updatedata')->name('update_trucks');
-    Route::get('/delete_trucks', 'trucksController@deletedata')->name('delete_trucks');
-
-    Route::get('/commodity', 'commodityController@index')->name('commodity');
-    Route::post('/add_commodity', 'commodityController@store')->name('add_commodity');
-    Route::get('/refresh_commodity', 'commodityController@refresh')->name('refresh_commodity');
-    Route::get('/update_commodity', 'commodityController@updatedata')->name('update_commodity');
-    Route::get('/delete_commodity', 'commodityController@deletedata')->name('delete_commodity');
-
-    Route::get('/users', 'usersController@index')->name('users');
-    Route::post('/add_user', 'usersController@store')->name('add_user');
-    Route::get('/refresh_user', 'usersController@refresh')->name('refresh_user');
-    Route::get('/update_user', 'usersController@updatedata')->name('update_user');
-    Route::get('/delete_user', 'usersController@deletedata')->name('delete_user');
-
-    Route::get('/roles', 'RolesController@index')->name('roles');
-    Route::post('/add_role', 'RolesController@store')->name('add_role');
-    Route::get('/refresh_roles', 'RolesController@refresh')->name('refresh_roles');
-    Route::get('/update_role', 'RolesController@updatedata')->name('update_role');
-    Route::get('/delete_role', 'RolesController@deletedata')->name('delete_role');
-
-    //SEARCH AUTOCOMPLETE NAME FOR EXPENSES
-    Route::get('autocomplete_name',array('as'=>'autocomplete_name','uses'=>'expenseController@autoComplete'));
-
-    Route::post('/print_trip', 'pdfController@trips')->name('print_trip');
-    Route::post('/print_expense', 'pdfController@expenses')->name('print_expense');
-    Route::post('/print_dtr', 'pdfController@dtr')->name('print_dtr');
-    Route::post('/print_od', 'pdfController@od')->name('print_od');
-    Route::post('/print_ca', 'pdfController@ca')->name('print_ca');
-    Route::post('/print_purchase', 'pdfController@purchases')->name('print_purchase');
-    Route::post('/print_sales', 'pdfController@sales')->name('print_sales');
-    Route::post('/print_balance_payment', 'pdfController@balance_payment')->name('print_balance_payment');
 });
 
-Route::group(['middleware'], function()
-{
+Route::group(['middleware'=>['auth']], function() {
     Route::get('/', function () {
         return redirect('/home');
     });
