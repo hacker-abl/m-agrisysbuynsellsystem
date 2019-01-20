5ae8c46a76742c2d1452d05d25dd44bc2198b842
Floyd Francis Matabilas <floydignified@gmail.com>
2019-01-21 00:03:15 +0800

    Floyd > add magri on print receipt

--- app/Http/Controllers/pdfController.php
+++ app/Http/Controllers/pdfController.php
@@ -28,23 +28,23 @@ class pdfController extends Controller
 
 		if($name == "trips"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,330);
+			$customPaper = array(0,0,200,360);
 			$dompdf->set_paper($customPaper);
 		}else if($name == "expense"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,300);
+			$customPaper = array(0,0,200,330);
 			$dompdf->set_paper($customPaper);
 		}else if($name == "ca"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,300);
+			$customPaper = array(0,0,200,330);
 			$dompdf->set_paper($customPaper);
 		}else if($name == "od"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,350);
+			$customPaper = array(0,0,200,380);
 			$dompdf->set_paper($customPaper);
 		}else if($name == "dtr"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,330);
+			$customPaper = array(0,0,200,350);
 			$dompdf->set_paper($customPaper);
 		}else if($name == "sales"){
 			// (Optional) Setup the paper size and orientation
@@ -52,11 +52,11 @@ class pdfController extends Controller
 			$dompdf->set_paper($customPaper);
 		}else if($name == "purchases"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,475);
+			$customPaper = array(0,0,200,505);
 			$dompdf->set_paper($customPaper);
 		}else if($name == "balance_payment"){
 			// (Optional) Setup the paper size and orientation
-			$customPaper = array(0,0,200,210);
+			$customPaper = array(0,0,200,240);
 			$dompdf->set_paper($customPaper);
 		}
 		
@@ -105,6 +105,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>TRIPS</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -176,6 +177,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>EXPENSE</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -236,6 +238,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>DAILY TIME RECORD</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -315,6 +318,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>OUTBOUND DELIVERIES</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -410,6 +414,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>CASH ADVANCE</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -480,6 +485,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>PURCHASE</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -586,6 +592,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>SALES</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
@@ -667,6 +674,7 @@ class pdfController extends Controller
 		</head>
 		<body>
 		<basefont size='4'>
+		<h1 align='center'>M-AGRI</h1>
 		<h2 align='center'>BALANCE PAYMENT</h2>
 		<p align='center'><b>P-1B Sto. Niño Carmen<br>
 		Davao del Norte</b> <br> ".Carbon::now()->toDayDateTimeString()."</p>
