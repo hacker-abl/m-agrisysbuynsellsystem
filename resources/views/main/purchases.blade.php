86be89bd394705f1daef68306e6eb570b24be5d3
Jan Rey Suarez <33534250+w33haa2@users.noreply.github.com>
2019-05-16 14:12:16 +0800

    Update purchases.blade.php

--- resources/views/main/purchases.blade.php
+++ resources/views/main/purchases.blade.php
@@ -829,10 +829,10 @@
             $('#partial').val('')
         }
         else {
-            ca_to_partial = ca > 0 ? ca - partial : partial * -1
+            ca_to_partial = ca > 0 ? ca - partial : partial
         }
         amtpay  = ca_to_partial + total 
-        balance =  prev + ca_to_partial 
+        balance =  prev + ca - partial
         $('#total').val(parseFloat(total).toFixed(2))
         $('#amount').val(parseFloat(amtpay).toFixed(2))
         $('#balance').val(parseFloat(balance).toFixed(2))
