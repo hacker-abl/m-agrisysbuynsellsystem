ea99ac3d70a39e1dc700fb45e37ab606e1c98443
Floyd Matabilas <floydignified@gmail.com>
2020-01-02 21:32:41 +0800

    Floyd > rework into table format

--- resources/views/home_content/commodity_list.blade.php
+++ resources/views/home_content/commodity_list.blade.php
@@ -1,11 +1,21 @@
 <div>
-<ul class="dashboard-stat-list">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Regular Price</th>
+            <th>Discounted Price</th>
+        </tr>
+        </thead>
+        <tbody>
+
     @foreach($commodityList as $commodity)
-        <li>
-        {{ $commodity->name }}
-        <span class="pull-right"><b>&#8369; {{ number_format($commodity->price, 2, '.', ',') }}</b> <small>&#8369; {{ number_format($commodity->suki_price, 2, '.', ',') }}</small></span>
-        </li>
+        <tr>
+            <td>{{ $commodity->name }}</td>
+            <td><b>&#8369; {{ number_format($commodity->price, 2, '.', ',') }}</b></td>
+            <td>&#8369; {{ number_format($commodity->suki_price, 2, '.', ',') }}</td>
+        </tr>
     @endforeach
-</ul>
-<!-- {{ $commodityList->links() }} -->
+        </tbody>
+    </table>
 </div>
--- resources/views/home_content/total_purchases_today.blade.php
+++ resources/views/home_content/total_purchases_today.blade.php
@@ -1,11 +1,36 @@
-
-<ul class="dashboard-stat-list">
-    @foreach($topCommoditiesToday as $commodity)
-    <li>
-        @foreach($commodity->commodityName as $comName)
-            {{$comName->name}}
+<div class="container">
+<br>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>COMMODITY</th>
+            <th>NET WEIGHT (Kilos)</th>
+            <th>TOTAL PRICE</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($topCommoditiesToday as $commodity)
+        <tr>
+            <td>
+                @foreach($commodity->commodityName as $comName)
+                    <span>{{$comName->name}}</span>
+                @endforeach
+            </td>
+            <td>
+            <span>{{$commodity->net}}</span>
+            </td>
+            <td>
+            <span><b>&#8369; {{ number_format($commodity->total, 2, '.', ',') }}</b></span>
+            </td>
+        </tr>
         @endforeach
-        <span class="pull-right"><b>&#8369; {{ number_format($commodity->total, 2, '.', ',') }}</b></span>
-    </li>
-    @endforeach
-</ul>
+    </tbody>
+    <tfoot>
+        <tr class="text-danger">
+            <th>TOTAL</th>
+            <th>{{$topCommoditiesTodayTotals['net']}}</th>
+            <th>&#8369; {{number_format($topCommoditiesTodayTotals['total'], 2, '.', ',')}}</th>
+        </tr>
+    </tfoot>
+</table>
+</div>
