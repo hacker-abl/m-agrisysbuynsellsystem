8f09ba4e00f655f0e69292f8e6fd13dfb3a1ddde
Floyd Matabilas <floydignified@gmail.com>
2020-01-02 21:31:48 +0800

    Floyd > UI overhaul

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -8,7 +8,20 @@
 
     <!-- Widgets -->
     <div class="row clearfix">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="card">
+                <div class="header text-center">
+                    <h2>PURCHASES TODAY</h2>
+                </div>
+                <div class="body" style="max-height:500px;">
+                    <section class="totalPurchases">
+                    @include('home_content.total_purchases_today')
+                    </section>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
             <div class="info-box bg-pink hover-expand-effect">
                 <div class="icon">
                     <i class="material-icons">show_chart</i>
@@ -51,42 +64,19 @@
                     <total-balance-today></total-balance-today>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
     <!-- #END# Widgets -->
 
     <div class="row clearfix">
-        <!-- Visitors -->
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="card">
-                <div class="body bg-pink" style="height:291px;">
-                    <div class="font-bold m-b--35">TOTAL PURCHASES</div>
-                    <section class="totalPurchases">
-                    @include('home_content.total_purchases')
-                    </section>
-                </div>
-            </div>
-        </div>
-        <!-- #END# Visitors -->
-        
-        <!-- Latest Social Trends -->
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="card">
-                <div class="body bg-teal" style="height:291px;">
-                    <div class="font-bold m-b--35">TOTAL PURCHASES - TODAY</div>
-                    <section class="totalPurchases">
-                    @include('home_content.total_purchases_today')
-                    </section>
-                </div>
-            </div>
-        </div>
-        <!-- #END# Latest Social Trends -->
-        
+
         <!-- Commodity List -->
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <div class="card">
-                <div class="body bg-cyan" style="height:291px;">
-                    <div class="font-bold m-b--35">COMMODITY LIST</div>
+                <div class="header text-center">
+                    <h2>COMMODITY PRICE LIST</h2>
+                </div>
+                <div class="body" style="min-height:150px; max-height:400px;">
                     <section class="commodityList">
                     @include('home_content.commodity_list')
                     </section>
@@ -95,99 +85,93 @@
         </div>
         <!-- #END# Commodity List -->
 
-    </div>
-
-    <div class="row clearfix">
-        <!-- Task Info -->
-        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+        <!-- Cashier List -->
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <div class="card">
-                <div class="header">
-                    <h2>PAYMENT LOGS</h2>
-                    <ul class="header-dropdown m-r--5">
-                        <li class="dropdown">
-                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <i class="material-icons">more_vert</i>
-                            </a>
-                            <ul class="dropdown-menu pull-right">
-                                <li><a href="javascript:void(0);">Action</a></li>
-                                <li><a href="javascript:void(0);">Another action</a></li>
-                                <li><a href="javascript:void(0);">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <section class="paymentLogs">
-                    @include('home_content.payment_logs_table')
-                </section>
-            </div>
-        </div>
-        <!-- #END# Task Info -->
-        <!-- Browser Usage -->
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="card">
-                <div class="header">
+                <div class="header text-center">
                     <h2>CASHIER LIST</h2>
                 </div>
-                <div class="body">
+                <div class="body" style="min-height:150px; max-height:400px;">
                     <cash-on-hand></cash-on-hand>
                 </div>
             </div>
         </div>
-        <!-- #END# Browser Usage -->
+        <!-- #END# Cashier List -->
     </div>
 
-    <!-- Widgets -->
     <div class="row clearfix">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <!-- Cash Advance -->
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="card">
+                <div class="header text-center">
+                    <h2>CASH ADVANCE TODAY</h2>
+                </div>
+                <div class="body" style="min-height:150px; max-height:400px;">
+                    <section class="paymentLogs">
+                        @include('home_content.cash_advance_today_table')
+                    </section>
+                </div>
+            </div>
+        </div>
+        <!-- #END# Cash Advance -->
+
+        <!-- Payment Logs -->
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="card">
+                <div class="header text-center">
+                    <h2>PAYMENT LOGS</h2>
+                </div>
+                <div class="body" style="min-height:150px; max-height:400px;">
+                    <section class="paymentLogs">
+                        @include('home_content.payment_logs_table')
+                    </section>
+                </div>
+            </div>
+        </div>
+        <!-- #END# Payment Logs -->
+    </div>
+
+    <div class="row clearfix">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
             <div class="info-box bg-pink hover-expand-effect">
                 <div class="icon">
                     <i class="material-icons">show_chart</i>
                 </div>
                 <div class="content">
-                    <div class="text">EXPENSES - MONTH</div>
-                    <h4 id="totalExpensesMonth">&#8369; {{ number_format( $finalTotalExpenseMonth , 2, '.', ',') }}</h4>
+                    <div class="text">EXPENSES - TODAY</div>
+                    <total-expenses-today></total-expenses-today>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
             <div class="info-box bg-cyan hover-expand-effect">
                 <div class="icon">
                     <i class="material-icons">shopping_cart</i>
                 </div>
                 <div class="content">
-                    <div class="text">SALES - MONTH</div>
-                    <h4 id="totalSalesMonth">&#8369; {{ number_format( $totalSalesMonth[0]->total_sales , 2, '.', ',') }}</h4>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box bg-light-green hover-expand-effect">
-                <div class="icon">
-                    <i class="material-icons">bookmark_border</i>
-                </div>
-                <div class="content">
-                    <div class="text">PURCHASES - MONTH</div>
-                    <h4 id="totalPurchasesMonth">&#8369; {{ number_format( $totalPurchasesMonth[0]->total_purchases , 2, '.', ',') }}</h4>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box bg-orange hover-expand-effect">
-                <div class="icon">
-                    <i class="material-icons">money_off</i>
-                </div>
-                <div class="content">
-                    <div class="text">BALANCE - MONTH</div>
-                    <h4 id="totalBalanceMonth">&#8369; {{ number_format( $totalBalanceMonth , 2, '.', ',') }}</h4>
+                    <div class="text">SALES - TODAY</div>
+                    <total-sales-today></total-sales-today>
                 </div>
             </div>
         </div>
     </div>
-    <!-- #END# Widgets -->
 
-    <!-- Widgets -->
     <div class="row clearfix">
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="col-md-6">
+            <div class="row clearfix">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="info-box bg-pink hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">show_chart</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">EXPENSES - MONTH</div>
+                            <h4 id="totalExpensesMonth">&#8369; {{ number_format( $finalTotalExpenseMonth , 2, '.', ',') }}</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="info-box bg-pink hover-expand-effect">
                         <div class="icon">
                             <i class="material-icons">show_chart</i>
@@ -198,7 +182,20 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            </div>
+            <div class="row clearfix">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="info-box bg-cyan hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">shopping_cart</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">SALES - MONTH</div>
+                            <h4 id="totalSalesMonth">&#8369; {{ number_format( $totalSalesMonth[0]->total_sales , 2, '.', ',') }}</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="info-box bg-cyan hover-expand-effect">
                         <div class="icon">
                             <i class="material-icons">shopping_cart</i>
@@ -209,31 +206,27 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-light-green hover-expand-effect">
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="info-box bg-light-green hover-expand-effect" style="min-height:190px; max-height:400px;">
                         <div class="icon">
                             <i class="material-icons">bookmark_border</i>
                         </div>
-                        <div class="content">
-                            <div class="text">PURCHASES - YEAR</div>
-                            <h4 id="totalPurchasesYear">&#8369; {{ number_format( $totalPurchasesYear[0]->total_purchases , 2, '.', ',') }}</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-orange hover-expand-effect">
-                        <div class="icon">
-                            <i class="material-icons">money_off</i>
-                        </div>
-                        <div class="content">
-                            <div class="text">BALANCE - YEAR</div>
-                            <h4 id="totalBalanceYear">&#8369; {{ number_format( $totalBalanceYear , 2, '.', ',') }}</h4>
+                        <div class="content" style="width:100%;">
+                            <h3>PURCHASES - MONTH</h3>
+                            <br>
+                            <h4>WEIGHT (KG) <span class="pull-right">{{ $totalPurchasesMonth[0]->total_kilos }}</span></h4>
+                            <h4>AMOUNT <span class="pull-right">&#8369; {{ number_format( $totalPurchasesMonth[0]->total_purchases , 2, '.', ',') }}</span></h4>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- #END# Widgets -->
+        </div>
+    </div>
 
 </div>
 @endsection
-
