f66e766d94faf00b51ac0be15acb4ae80c8a5ee1
Benedict Badilles <benbenbadilles@gmail.com>
2020-08-19 09:36:55 +0800

    mao n

--- resources/views/main/trip.blade.php
+++ resources/views/main/trip.blade.php
@@ -195,7 +195,9 @@
                     <h2>List of Pick Ups as of {{ date('Y-m-d ') }}</h2>
                         <ul class="header-dropdown m-r--5">
                             <li class="dropdown">
+                                @if(isAdmin() || isPurchaser())
                                 <button type="button" class="btn bg-grey btn-xs waves-effect m-r-20 open_pickup_modal"><i class="material-icons">library_add</i></button>
+                                @emdof
                             </li>
                         </ul>
                 </div>
